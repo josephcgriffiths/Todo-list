cb16d12e0cdc5262e0fa12890aad8ef549bcb4cf
joseph <josephcg.14@gmail.com>
2025-09-28 12:39:25 +0100

    added remove

--- main.c
+++ main.c
@@ -32,14 +32,63 @@ void add_task(nt *head, char *val){
 	
 }
 
+char* pop(nt **head){
+	char* retval = NULL;
+	nt *new_node = NULL;
+
+	if(*head == NULL){
+		return "error: no todo list";
+	}
+
+	new_node = (*head)->next;
+	retval = (*head)->task;
+	free(*head);
+	*head = new_node;
+	return retval;
+	
+}
+
+char* delete_task(nt **head, int n){
+	int i = 0;
+	char* retval = NULL;
+	nt *current = *head;
+	nt *temp_node = NULL;
+
+	if (n == 0){
+		pop(head);
+	}
+
+	for (i=0;i<n-1;i++){
+		if(current->next == NULL){
+			return "error: no item in the list";
+		}
+		current = current->next;
+	}
+
+	if(current->next == NULL){
+		return "error: no item in the list";
+	}
+
+	temp_node = current->next;
+	retval = current->task;
+	current->next = temp_node->next;
+	free(temp_node);
+	return retval;
+}
+
 int main(){
 	nt *head = NULL;
 	head = malloc(sizeof(nt));
+	head->task = "Create a new list";
+	head->next = malloc(sizeof(nt));
+	head->next->task = "Create an item";
+	head->next->next = NULL;
 	
 	int option;
 
 	printf("Please select an option:\n");
 	printf("1: Add a task\n");
+	printf("2: Delete a task\n");
 
 	scanf("%d", &option);
 
@@ -51,8 +100,17 @@ int main(){
 		fgets(val, 20, stdin);
 		val[strcspn(val,"\n")] = '\0';
 		add_task(head, val);
-	}
+	} else if (option == 2){
+		print_list(head);
+		printf("Enter the index to delete: ");
+		int index;
+		scanf("%d", &index);
+		while(getchar() != '\n') {}
 
+		printf("You have deleted the task: \n");
+		delete_task(&head, index);
+	}
+	
 	print_list(head);
 	
 }
